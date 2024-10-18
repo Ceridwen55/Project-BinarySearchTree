247fb0652e78ef7c2d7e3a2604a9695785bee12d
Zaqzulkan <zulkanzaqzuq@gmail.com>
2024-10-18 21:15:04 +0700

    Done with binary

--- bst.js
+++ bst.js
@@ -1,22 +1,49 @@
 import { Node } from "./node.js";
 
-function tree (array)
-{   
-    return { buildTree(array,0,array.length-1) }
+class tree {
+    constructor(array){
+        this.root = this.buildTree(array, 0, array.length-1);
+    }
 
-}
+
 buildTree(array,start,end)
 {
-    if (start > end) return null;
+    if (start > end) 
+        {
+            return null;
+        }
 
     let mid = Math.floor((start + end) / 2);
-    let root = new Node(arr[mid]);
+    let root = Node(array[mid]);
 
-    root.left = buildTree(array,start,mid-1);
-    root.right = buildTree(array,mid + 1, end);
+    root.left = this.buildTree(array,start,mid-1);
+    root.right = this.buildTree(array,mid + 1, end);
 
     return root;
 }
+
+};
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) 
+    {
+        return;
+    }
+    if (node.right !== null) 
+    {
+        prettyPrint(node.right, `${prefix}${isLeft ? "|   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "+-- " : "+-- "}${node.data}`);
+    if (node.left !== null) 
+    {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "|   "}`, true);
+    }
+};
+
+  const test = new tree([1,2,3,4,5,6,7,8,9,10]);
+  prettyPrint(test.root);
+
+ 
 
 
     
