f5118283f8cea50b71f6a6cfa7baf4897bb261a1
Zaqzulkan <zulkanzaqzuq@gmail.com>
2024-10-18 21:56:48 +0700

    4

--- bst.js
+++ bst.js
@@ -21,6 +21,99 @@ buildTree(array,start,end)
 
     return root;
 }
+// insert another number into the bst
+insert(value)
+{
+    if(this.root === null) 
+    {
+        this.root = Node(value)
+        return;
+    }
+    let currentNode = this.root;
+    let parentNode = null;
+
+    while(currentNode !== null)
+    {   parentNode = currentNode;
+
+        if(value < currentNode.data)
+        {
+            currentNode = currentNode.left;
+        }
+        else if (value > currentNode.data)
+        {
+            currentNode = currentNode.right;
+        }
+        else
+        {
+            return;
+        }
+
+
+    }
+
+    if(value < parentNode.data)
+    {
+        parentNode.left = Node(value);
+    }
+    else
+    {
+        parentNode.right = Node(value);
+    }
+    
+}
+
+deleteItem(value) 
+{
+    this.root = this.deleteNode(this.root, value);
+}
+
+deleteNode(node, value) 
+{
+    if (node === null) 
+    {
+        return node; 
+    }
+
+    if (value < node.data) 
+    {
+        node.left = this.deleteNode(node.left, value); 
+    } else if (value > node.data) 
+    {
+        node.right = this.deleteNode(node.right, value); 
+    } else 
+    {
+        
+
+        
+        if (node.left === null) 
+        {
+            return node.right; 
+        } else if (node.right === null) 
+        {
+            return node.left; 
+        }
+
+        
+        let successor = this.findMin(node.right);
+
+        
+        node.data = successor.data;
+
+        
+        node.right = this.deleteNode(node.right, successor.data);
+    }
+
+    return node; 
+}
+
+findMin(node) 
+{
+    while (node.left !== null) 
+    {
+        node = node.left; 
+    }
+    return node; 
+}
 
 };
 
@@ -41,6 +134,8 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 };
 
   const test = new tree([1,2,3,4,5,6,7,8,9,10]);
+  test.insert(80);
+  test.insert(7);
   prettyPrint(test.root);
 
  
